6cf4e1f8c8b417dbb1ac46d797963df5d33542aa
Buu342 <buu342@hotmail.com>
2023-12-01 20:35:18 +0000

    Added my stuff + frequency analyzer

---
+++ autoloads/MusicController.gd
@@ -0,0 +1,41 @@
+extends Node
+
+var track = load("res://Music/Mixdown.mp3")
+const VU_COUNT = 16
+const FREQ_MAX = 11050.0
+
+const WIDTH = 400
+const HEIGHT = 100
+
+const MIN_DB = 60
+
+var spectrum
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func InitSpectrum():
+	spectrum = AudioServer.get_bus_effect_instance(0,0)
+	print(spectrum)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+func GetFrequency(val):
+	# Val should be between 0 and VU_COUNT
+	var w = WIDTH / VU_COUNT
+	var prev_hz = 0
+	var hz = val * FREQ_MAX / VU_COUNT;
+	var magnitude: float = spectrum.get_magnitude_for_frequency_range(prev_hz, hz).length()
+	var energy = clamp((MIN_DB + linear_to_db(magnitude)) / MIN_DB, 0, 1)
+	var height = energy * HEIGHT
+	print(height)
+	prev_hz = hz
+
+func play_music():
+	print("Playing music")
+	$Music.stream = track
+	$Music.play()
+	
